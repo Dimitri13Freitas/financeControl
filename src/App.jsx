7739da87f153342a95bda7224ba97915eb0da03b
Dimitri Freitas <freitasdimitri7@gmail.com>
2023-04-26 00:29:36 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Controle de finanças</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
+
